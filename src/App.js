6ba0e0aca5bbb6f52f815fa0067bfa2cd93bbc65
Inthone Vilayhong <Thonouu@gmail.com>
2022-04-27 20:29:52 +0200

    first-commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
         <div>
             <div className="header">
                 <i class="fa-solid fa-rocket rocket"></i>
-                <h1>React Counter</h1>
+                <h1>Ready To Go</h1>
             </div>
 
             <div className="container">
